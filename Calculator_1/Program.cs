021933d62e4cd514a85e3363395af5263caa01f3
Oleksandr.Reshotka <oleksandr.reshotka@schindler.com>
2020-04-14 15:04:10 +0300

    init. commit

---
+++ Calculator_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator_1
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! This is a calculator program, v1.");
+
+            double a;
+            double b;
+            double total;
+            char oper;
+
+            Console.WriteLine("Enter the first number:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine("The result is: " + total + ".");
+            }
+
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("The result is: " + total + ".");
+            }
+
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("The result is: " + total + ".");
+            }
+
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine("The result is: " + total + ".");
+            }
+            else
+            {
+                Console.WriteLine("Unknown operator.");
+            }
+        }
+    }
+}
